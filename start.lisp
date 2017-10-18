bce57dc957247dee3ad4e75827ce157c4965f40f
paule32 <jkallup@web.de>
2017-10-18 20:35:29 +0200

    update

--- start.lisp
+++ start.lisp
@@ -4,18 +4,25 @@
 ;; non-profit
 ;; -------------------------------------------------
 
-(defparameter wordtable '(
-  ("Auto"                ("s" 1) () )
-  ("Haus"                ("s" 2) () )
-  ("Maus"                ("s" 3) ("Säugetier") )
-  ("Vogel"               ("s" 7) ("Lebewesen") )  
-  ("Person"              ("s" 4) () )
-  ("Säugetier"           ("s" 5) ("Lebewesen") )
-  ("Unternehmen"         ("s" 6) () ))
-)
+(defvar false 0)
+(defvar true  1)
+(defvar found false)
 
-(print (second (first wordtable)))
-(print (second (list (quote("Maus" 10)) (quote("Tweety" 22)))))
+(defparameter kann-table '(
+  ("gehen")
+))
+
+(defparameter ist-table '(
+  ("säugetier" ("vogel"))
+))
+
+(defparameter word-table '(
+  ("vogel" (ist kann hat))
+  ("maus"  (koennte))
+))
+
+;(print (second (first word-table)))
+;(print (second (list (quote("Maus" 10)) (quote("Tweety" 22)))))
 
 ;; -----------------------------------------------------------
 ;; Returns a list of substrings of string
@@ -29,33 +36,30 @@
           collect (subseq string i j)
           while j))
 
+(defun mashup-symbol (&rest objects)
+  (intern
+  (format nil "~{~a~}"  objects)))
+
+(defun list-to-string (lst)
+    (format nil "~{~A~}" lst))
+
 (defun main()
   (format T "~%Bitte stellen Sie ihre Frage:~%")
   (force-output)
   (let ((input-text (read-line)))
     (setf input-text (split-by-one-space input-text))
-    (print input-text)
-    (setf txt1 (first  input-text)) ;; "was"
-    (setf txt2 (second input-text)) ;; "ist"
-    (setf txt3 (third  input-text)) ;; key = maus
-    (if (string= txt1 "was")(progn
-      (if (string= txt2 "ist")(progn
-      (loop for key in wordtable do
-      (setf src1 (string-equal (first key) txt3))
-      (if (string= src1 T) (progn
-        (print "gefunden: ")
-        (princ (first key))
-        (princ " ist ")
-        (princ (third key))
-        (setf src2 (third key)))))
-      (loop for key2 in wordtable do
-(setf src3 (first key2))
-(setf src4 (first src2))
-(if (string-equal src3 src4) (progn
-(print "OK"))
-            )))))))
-  (print "end.")
+    (loop for x in word-table
+      do (progn
+        (setq found (string-equal (first x) (first input-text)))
+      )))
+      (cond (found nil
+        (progn
+            (print "ok")
+            (return-from main 1)
+        )   ))
 )
+
+(print (mashup-symbol "bird " (symbol-name 'KANN) '-TABLE))
 
 (main)
 (exit)
